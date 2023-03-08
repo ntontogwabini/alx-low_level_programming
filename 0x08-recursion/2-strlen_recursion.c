6d84901072dd8a36095cf880eadfb016d30fe661
Nompumelelo <mpumejoko@gmail.com>
2023-03-08 11:25:22 -0800

    Update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,25 +1,19 @@
 #include "main.h"
-
 /**
- * _print_rev_recursion - function declaration
- * @s: the string
- * Return: void
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
  */
-
-void _print_rev_recursion(char *s);
-
-/**
- * _print_rev_recursion - function definition
- * @s: the string to recurse through
- * Description: print a string in reverse using recursion
- * Return: void
- */
-
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
+	int longit = 0;
+
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
+
+	return (longit);
 }
