93d115402122c1a9afb97e20bee3e0d33c6621e2
Nompumelelo <mpumejoko@gmail.com>
2023-03-13 10:45:18 -0700

    Update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,51 +1,79 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
+		
+#include <stdlib.h>
+		
 /**
- * argstostr - function declaration
- * @ac: no of args
- * @av: array of pointer
- * Return: char type
+		
+ * argstostr - main entry
+		
+ * @ac: int input
+		
+ * @av: double pointer array
+		
+ * Return: 0
+		
  */
-
-char *argstostr(int ac, char **av);
-
-/**
- * argstostr - function definition
- * @ac: number of arguments
- * @av: array of pointers to argumnets
- * Description: Return NULL of pointer to string
- * Return: NULL or a pointer to a string
- */
-
+		
 char *argstostr(int ac, char **av)
+		
 {
-	int row, column, len = 0, total_len = 0;
+		
+	int i, n, r = 0, l = 0;
+		
 	char *str;
+		
 
+		
 	if (ac == 0 || av == NULL)
+		
 		return (NULL);
+		
 
-	for (row = 0; row < ac; row++)
+		
+	for (i = 0; i < ac; i++)
+		
 	{
-		len = strlen(av[row]);
-		total_len += len + 1;
+		
+		for (n = 0; av[i][n]; n++)
+		
+			l++;
+		
 	}
+		
+	l += ac;
+		
 
-	str = (char *) malloc(total_len * sizeof(char));
+		
+	str = malloc(sizeof(char) * l + 1);
+		
 	if (str == NULL)
+		
 		return (NULL);
-
-	len = 0;
-	for (row = 0; row < ac; row++)
+		
+	for (i = 0; i < ac; i++)
+		
 	{
-		for (column = 0; av[row][column] != '\0'; column++)
-		{
-			str[len++] = av[row][column];
-		}
-		str[len++] = '\n';
+		
+	for (n = 0; av[i][n]; n++)
+		
+	{
+		
+		str[r] = av[i][n];
+		
+		r++;
+		
 	}
-	str[len] = '\0';
+		
+	if (str[r] == '\0')
+		
+	{
+		
+		str[r++] = '\n';
+		
+	}
+		
+	}
+		
 	return (str);
+		
 }
